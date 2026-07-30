c81b9011ba0fb73d14e0220a4b09b965ad82f497
HTH <HTH>
2026-07-30 21:12:47 +0700

    @

---
+++ RDLC/wwwroot/js/reports.js
@@ -0,0 +1,28 @@
+﻿const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+const url = new URL(document.querySelector('meta[name="current-page-path"]').content, location.origin);
+url.searchParams.set('handler', 'SelectReport');
+
+const viewerUrl = new URL(document.querySelector('#ifrReport').src);
+
+async function selectReport(element) {
+    url.searchParams.set('reportName', element.dataset.reportName);
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-XSRF-TOKEN': csrfToken
+        },
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+
+        viewerUrl.searchParams.set('id', result.reportId);
+        viewerUrl.searchParams.set('name', result.reportName);
+
+        document.querySelector('#ifrReport').src = viewerUrl;
+    } else {
+        alert('Failed to select report.');
+    }
+}
